cfd50716abbdd1baa2ccad4a0fafdf602801f597
royalranjit <ranjitchaudhary5209@gmail.com>
2023-02-26 17:31:43 +0545

    first react commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import './Style.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classNameName="App">
+      <div className="container">
+  <nav>
+    <div className="navbar">
+      <div className="logo">
+        <h2>Ranjit</h2>
+      </div>
+      <div className="details">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="/landingPage/contact.html">Contact</a></li>
+          <li><a href="/landingPage/hello.html">About</a></li>
+        </ul>
+      </div>
+  </div>
+  </nav>
+  <div className="animation"></div>
+  <div className="bodys">
+    <div className="info">
+      <div className="my-info">
+        <h1>I am Computer Engineer</h1>
+        <p>i am student of Nepal college of information Technology</p>
+      </div>
+      <div className="buttons">
+        <button><a href="https://www.facebook.com/ranjitchaudhary.5209/">Facebook</a></button>
+        <button>intagram</button>
+      </div>
+    </div>
+    
+  </div>
+</div>
     </div>
   );
 }
